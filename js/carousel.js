8e796649156aaa895f3b41429a313557af634344
Ben Israeli <jisraeli@gmail.com>
2013-07-25 00:31:51 -0400

    hover and exit opacity change is good, but can't get carousel working properly for toright and toleft functions

--- js/carousel.js
+++ js/carousel.js
@@ -1,12 +1,49 @@
-var carousel = document.getElementById('carousel');
+window.onload = function() {
 
-// Buttons
-var next = document.getElementById('next');
-var previous = document.getElementById('previous');
+  var carousel = document.getElementById('carousel');
+  var canvas = document.getElementById('window');
+  // Buttons
+  var next = document.getElementById('next');
+  var previous = document.getElementById('previous');
 
-carousel.style.marginLeft = 0;
+  carousel.style.marginLeft = "0px";
 
-// Create mouseoveer and mouseexit events for the buttons
+  var darkenButtons = function(event){
+    next.style.opacity = 0.5;
+    previous.style.opacity = 0.5;
+  };
+
+  var lightenButtons = function(event){
+    next.style.opacity = 0.2;
+    previous.style.opacity = 0.2;
+  };
+
+  var toRight = function(event){
+      carousel.style.marginRight = "612px";
+  }
+
+  var toLeft = function(event){
+      carousel.style.marginLeft = "612px";
+  }
+
+  next.addEventListener("click", toRight, false);
+  previous.addEventListener("click", toLeft, false);
+
+  //next.onclick = moveRight;
+  // Create mouseoveer and mouseexit events for the buttons
+  carousel.addEventListener("mouseover", darkenButtons, false);
+  next.addEventListener("mouseover", darkenButtons, false);
+  previous.addEventListener("mouseover", darkenButtons, false);
+
+  carousel.addEventListener("mouseout", lightenButtons, false);
+  next.addEventListener("mouseout", lightenButtons, false);
+  previous.addEventListener("mouseout", lightenButtons, false);
+
+
+
+};
+
+
 // to change their opacity from .5 to .2
 
 // Create onclick events to advance the images
